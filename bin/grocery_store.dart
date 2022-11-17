bdaded09025336c3e1d7ced26cb3ffd2a48df2a1
G'ayrat <shirnboyevgayrat@gmail.com>
2022-11-17 13:46:20 +0500

    price

--- bin/grocery_store.dart
+++ bin/grocery_store.dart
@@ -4,10 +4,11 @@ import 'package:grocery_store/grocery_store.dart';
 
 void main() {
 //  Read the file
+
   var file = File('grocery.json');
   var contents = file.readAsStringSync();
 
   // Convert the contents to a map
   var groceryMap = jsonDecode(contents);
-  print(getTotal(groceryMap['products']));
+  print(getTotalPrices(groceryMap['products']));
 }
